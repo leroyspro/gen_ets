0671ea71cf5d62d4b3d3265cda9bdce6d9f5c901
centurion <centurionoff.dev@proton.me>
2024-03-30 18:08:09 -0300

    chore: configured mix file

--- mix.exs
+++ mix.exs
@@ -5,8 +5,17 @@ defmodule GenETS.MixProject do
     [
       app: :gen_ets,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      description: "Small wrapper around :ets Erlang/OTP library",
+      package: [
+        name: "gen_ets",
+        licenses: ["Apache-2.0"],
+        links: %{"GitHub" => "https://github.com/leroyspro/gen_ets"}
+      ],
+      name: "GenETS",
+      source_url: "https://github.com/leroyspro/gen_ets",
+      homepage_url: "https://github.com/leroyspro/gen_ets"
     ]
   end
 end
